a5c0519ac0b5bb92468ed63ebf21f6b852172fa3
Tyler Knapp <tylerknapp25@gmail.com>
2022-04-18 08:55:41 -0400

    Refactoring control flow statements and logic

--- src/main/java/com/twindo/TwindoApplication.java
+++ src/main/java/com/twindo/TwindoApplication.java
@@ -4,8 +4,11 @@ import java.io.IOException;
 
 public class TwindoApplication {
     
+    private static final String MENU_OPTION_ONE = "1";
+    private static final String MENU_OPTION_TWO = "2";
+    private static final String MENU_OPTION_THREE = "3";
+    
     private final Menu menu;
-    //private final Account account;
     private final CSVReader csvReader = new CSVReader();
     
     public TwindoApplication(Menu menu) {
@@ -14,34 +17,29 @@ public class TwindoApplication {
     
     public void run(){
         menu.welcomeMessage();
-        csvReader.readFromFile(menu.askForFile());
         controlFlow(menu.optionPageOne());
     }
     
     private void controlFlow(String option){
-        if(option.equalsIgnoreCase("1")){
-            //display items
-        }else if(option.equalsIgnoreCase("2")){
-            //order
-            String optionTwoInput = menu.optionPageTwo();
-            
+        if(option.equalsIgnoreCase(MENU_OPTION_ONE)){
+            csvReader.readFromFile(menu.askForFile());
+        }else if(option.equalsIgnoreCase(MENU_OPTION_TWO)){
+            menuTwoControlFlow(menu.optionPageTwo());
         }else{
             //this is where you quit out of our application
         }
     }
     
     private void menuTwoControlFlow(String option){
-        if(option.equalsIgnoreCase("1")){
+        if(option.equalsIgnoreCase(MENU_OPTION_ONE)){
             Account account = new Account();
             account.addMoneyToAccount(menu.addMoneyOption());
-        }else if(option.equalsIgnoreCase("2")){
+        }else if(option.equalsIgnoreCase(MENU_OPTION_TWO)){
             //Select Products
         }else{
-        
+            //Confirm Selection
         }
     }
-    
-    
     
     public static void main(String[] args){
         Menu menu = new Menu();
